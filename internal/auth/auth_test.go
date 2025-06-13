f11ef6e6161faded87c633dfd0ab65f02754ff63
MicGul <mwarioxin@gmail.com>
2025-06-13 08:03:05 +0200

    added tests for GetApiKey

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headerRandom := http.Header{}
+	headerRandom.Add("Authorization", "test test")
+
+	headerValid := http.Header{}
+	headerValid.Add("Authorization", "ApiKey qwe123456")
+
+	tests := []struct {
+		name string
+		header http.Header
+		apiKey string
+		wantErr bool
+	} {
+		{
+			name: "Empty Api key",
+			header: http.Header{},
+			apiKey: "",
+			wantErr: true,
+		},
+		{
+			name: "Missing ApiKey name",
+			header: headerRandom,
+			apiKey: "",
+			wantErr: true,
+		},
+		{
+			name: "Return Valid ApiKey",
+			header: headerValid,
+			apiKey: "qwe123456",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiKey, err := GetAPIKey(tt.header)
+			if apiKey != tt.apiKey {
+				t.Errorf("GetAPIKey() wanted %s, got %s", tt.apiKey, apiKey)
+			}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+
+
+}
